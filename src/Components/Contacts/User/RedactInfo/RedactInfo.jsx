e23ca08808d59cc724d9ae1d5c8cc1e6cc531c9b
drotsyk <43176477+drotsyk@users.noreply.github.com>
2021-05-12 13:16:25 +0300

    Update RedactInfo.jsx

--- src/Components/Contacts/User/RedactInfo/RedactInfo.jsx
+++ src/Components/Contacts/User/RedactInfo/RedactInfo.jsx
@@ -2,8 +2,8 @@ import React, {useState} from 'react'
 import { Button } from '../../../Button/Button'
 
 export const RedactInfo = ({toggleRedact, redact, redactInfo, item, sortUsers}) => {
-  const [user, setUser] = useState(" ")
-  const [phone, setPhone] = useState(" ")
+  const [user, setUser] = useState(item.name)
+  const [phone, setPhone] = useState(item.phone)
   return (
     <div className="pop-up__content">
       <img onClick={()=> toggleRedact(!redact)} className="pop-up__close" src="./Assets/cross.svg" alt="" />
@@ -13,7 +13,6 @@ export const RedactInfo = ({toggleRedact, redact, redactInfo, item, sortUsers}) 
       <form className="pop-up__form" action="" onSubmit={(e)=> {
         e.preventDefault()
         if(/^[a-zA-Z\u00C0-\u00ff]+\s+[a-zA-Z\u00C0-\u00ff]+$/.test(user) && /^((8|\+7)[- ]?)?(\(?\d{3}\)?[- ]?)?[\d\- ]{7,10}$/.test(phone)){
-          
           redactInfo(item.id, user.charAt(0).toUpperCase() + user.slice(1), phone)
           toggleRedact(!redact)
           sortUsers()
@@ -26,13 +25,15 @@ export const RedactInfo = ({toggleRedact, redact, redactInfo, item, sortUsers}) 
           className="pop-up__input" 
           placeholder="Contact Name" 
           maxLength="16" 
+          value={user}
           onChange={(e) => setUser(e.target.value)}
         />
-        <input 
+        <input
           type="text"
           className="pop-up__input"
           placeholder="Contact Phone"
-          maxLength="10"  
+          maxLength="10" 
+          value={phone} 
           onChange={(e) => setPhone(e.target.value)}
         />
         <Button text="Save" color="#06C668" />
